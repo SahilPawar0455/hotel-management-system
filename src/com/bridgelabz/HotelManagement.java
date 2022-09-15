3d2b420659393cc70ad89d508d60b7aaf4378bed
SAHIL0455 <sahilpawar1004@gmail.com>
2022-09-15 20:33:47 +0530

    Initial welcome Messages

--- src/com/bridgelabz/HotelManagement.java
+++ src/com/bridgelabz/HotelManagement.java
@@ -2,6 +2,6 @@ package com.bridgelabz;
 
 public class HotelManagement {
     public static void main(String[] args) {
-        System.out.println("Welcome to Hotel Management");
+        System.out.println(" Welcome to Hotel Management System");
     }
 }
